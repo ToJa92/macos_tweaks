ebd55350b31eddde33cd44a93b22a63438413615
Tobias Jansson <tobias_jansson@me.com>
2016-05-04 20:45:28 +0200

    Updated tweaks

--- applied.sh
+++ applied.sh
@@ -2,17 +2,17 @@
 
 # to undo: defaults delete <directory> <directive>
 
-echo "Enable subpixel font rendering on non-Apple LCDs"
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+#echo "Enable subpixel font rendering on non-Apple LCDs"
+#defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
 echo "Use current directory as default search scope in Finder"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-echo "Show Path bar in Finder"
-defaults write com.apple.finder ShowPathbar -bool true
+#echo "Show Path bar in Finder"
+#defaults write com.apple.finder ShowPathbar -bool true
 
-echo "Show Status bar in Finder"
-defaults write com.apple.finder ShowStatusBar -bool true
+#echo "Show Status bar in Finder"
+#defaults write com.apple.finder ShowStatusBar -bool true
 
 echo "Expand save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -45,8 +45,8 @@ echo "Show the ~/Library folder"
 chflags nohidden ~/Library
 
 echo "Make Dock show up faster"
-defaults write com.apple.dock autohide-time-modifier -float 0.5
-defaults write com.apple.Dock autohide-delay -float 0
+#defaults write com.apple.dock autohide-time-modifier -float 0.5
+#defaults write com.apple.Dock autohide-delay -float 0
 
 # Use list view in all Finder windows by default
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`, "Nlsv"
@@ -54,13 +54,24 @@ echo "Make column view default"
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 
 # Show indicator lights for open applications in the Dock
-echo "Show indicator lights for open applications in the Dock"
-defaults write com.apple.dock show-process-indicators -bool true
+#echo "Show indicator lights for open applications in the Dock"
+#defaults write com.apple.dock show-process-indicators -bool true
 
 # Set Desktop as the default location for new Finder windows
 # For other paths, use `PfLo` and `file:///full/path/here/`
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
-# Speed up Mission Control animations
-defaults write com.apple.dock expose-animation-duration -float 0.01
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Improve Bluetooth audio quality
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)" 80 
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 80 
+defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)" 80 
+defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool Min (editable)" 80 
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool" 80 
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Max" 80 
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Min" 80
